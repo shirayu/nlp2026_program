796737e5788138cd9391b158dba7f2d29b9051c6
Yuta Hayashibe <yuta@hayashibe.jp>
2026-03-04 19:36:31 +0900

    分割

--- src/pages/ProgramPage.tsx
+++ src/pages/ProgramPage.tsx
@@ -9,62 +9,7 @@ import { fullscreenDialogClassName, getNextScheduleTimePoint } from "./programPa
 export { SearchField, fullscreenDialogClassName, getNextScheduleTimePoint };
 
 export default function ProgramPage() {
-  const {
-    data,
-    settings,
-    toggleShowAuthors,
-    toggleUseSlackAppLinks,
-    bookmarkedPresentationIds,
-    bookmarkedSessionIds,
-    toggleBookmark,
-    toggleSessionBookmark,
-    query,
-    selectedDate,
-    selectedRoom,
-    selectedTime,
-    searchAll,
-    showFilters,
-    showSettings,
-    showBookmarkedOnly,
-    showInstallDialog,
-    personModal,
-    installContext,
-    installPromptEvent,
-    mainRef,
-    settingsDialogRef,
-    installDialogRef,
-    sessionRefs,
-    sessionSlackLinks,
-    slackAppUrl,
-    slackWebUrl,
-    allDates,
-    allRooms,
-    allTimes,
-    timelineSegments,
-    filteredSessions,
-    trimmedQuery,
-    isSearching,
-    sessionsVisible,
-    filtersDisabled,
-    searchScopeLabel,
-    nowEnabled,
-    bookmarkCount,
-    matchedPresentationCount,
-    setQuery,
-    setSearchAll,
-    setShowFilters,
-    setShowSettings,
-    setShowInstallDialog,
-    setPersonModal,
-    handleJumpToSession,
-    handleToggleExpanded,
-    toggleBookmarkFilter,
-    selectDate,
-    selectTime,
-    selectRoom,
-    handleSelectNow,
-    handleInstallApp,
-  } = useProgramPageState();
+  const { data, headerProps, resultsProps, overlayProps } = useProgramPageState();
 
   if (!data) {
     return (
@@ -76,86 +21,9 @@ export default function ProgramPage() {
 
   return (
     <div className="flex h-screen flex-col bg-gray-50">
-      <ProgramHeader
-        query={query}
-        isSearching={isSearching}
-        searchAll={searchAll}
-        bookmarkCount={bookmarkCount}
-        bookmarkFilterActive={showBookmarkedOnly}
-        showSettings={showSettings}
-        showInstallButton
-        showInstallDialog={showInstallDialog}
-        slackUrl={settings.useSlackAppLinks ? slackAppUrl : slackWebUrl}
-        slackAppUrl={slackAppUrl}
-        useSlackAppLinks={settings.useSlackAppLinks}
-        allDates={allDates}
-        filtersDisabled={filtersDisabled}
-        selectedDate={selectedDate}
-        showFilters={showFilters}
-        allTimes={allTimes}
-        timelineSegments={timelineSegments}
-        selectedTime={selectedTime}
-        nowEnabled={nowEnabled}
-        rooms={allRooms}
-        selectedRoom={selectedRoom}
-        onQueryCommit={setQuery}
-        onToggleSearchAll={() => setSearchAll((value) => !value)}
-        onToggleBookmarkFilter={toggleBookmarkFilter}
-        onOpenSettings={() => setShowSettings(true)}
-        onOpenInstallDialog={() => setShowInstallDialog(true)}
-        onSelectDate={selectDate}
-        onToggleFilters={() => setShowFilters((value) => !value)}
-        onSelectTime={selectTime}
-        onSelectNow={handleSelectNow}
-        onSelectRoom={selectRoom}
-      />
-
-      <ProgramResults
-        data={data}
-        mainRef={mainRef}
-        trimmedQuery={trimmedQuery}
-        showBookmarkedOnly={showBookmarkedOnly}
-        searchScopeLabel={searchScopeLabel}
-        matchedPresentationCount={matchedPresentationCount}
-        filteredSessions={filteredSessions}
-        bookmarkedPresentationIds={bookmarkedPresentationIds}
-        bookmarkedSessionIds={bookmarkedSessionIds}
-        sessionSlackLinks={sessionSlackLinks}
-        showAuthors={settings.showAuthors}
-        sessionsVisible={sessionsVisible}
-        sessionRefs={sessionRefs}
-        onToggleExpanded={handleToggleExpanded}
-        onPersonClick={setPersonModal}
-        onJumpToSession={handleJumpToSession}
-        onToggleBookmark={toggleBookmark}
-        onToggleSessionBookmark={toggleSessionBookmark}
-      />
-
-      <ProgramOverlays
-        personModal={personModal}
-        data={data}
-        bookmarkedPresentationIds={bookmarkedPresentationIds}
-        showAuthors={settings.showAuthors}
-        onClosePersonModal={() => setPersonModal(null)}
-        onPersonClick={setPersonModal}
-        onJumpToSessionFromPerson={(sid) => {
-          setPersonModal(null);
-          handleJumpToSession(sid);
-        }}
-        onToggleBookmark={toggleBookmark}
-        installDialogRef={installDialogRef}
-        showInstallDialog={showInstallDialog}
-        installContext={installContext}
-        hasInstallPrompt={installPromptEvent !== null}
-        onCloseInstallDialog={() => setShowInstallDialog(false)}
-        onInstall={() => void handleInstallApp()}
-        settingsDialogRef={settingsDialogRef}
-        showSettings={showSettings}
-        useSlackAppLinks={settings.useSlackAppLinks}
-        onCloseSettings={() => setShowSettings(false)}
-        onToggleShowAuthors={toggleShowAuthors}
-        onToggleUseSlackAppLinks={toggleUseSlackAppLinks}
-      />
+      <ProgramHeader {...headerProps} />
+      <ProgramResults data={data} {...resultsProps} />
+      <ProgramOverlays data={data} {...overlayProps} />
     </div>
   );
 }
